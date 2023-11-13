e09bf7c8cf6760314c32ec4cc44835c6c50dff99
Arizidane <samionohari@gmail.com>
2023-11-13 19:35:28 +0700

    feat: features section

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,32 @@
 // <==========({[Card Animation Handler]})==========>
-function cardAnimation(selector, delay, duration = 7.5) {
+function cardAnimation(selector, animationName, delay, duration = 7.5) {
     document.querySelector(selector).style.visibility = 'hidden'
     setTimeout(() => {
-        document.querySelector(selector).style.animation = `loopPopUp ${duration}s infinite`
+        document.querySelector(selector).style.animation = `${animationName} ${duration}s infinite`
         document.querySelector(selector).style.visibility = 'visible'
     }, delay);
 }
 
 if (!window.matchMedia("(max-width: 768px)").matches) {
-    cardAnimation('.card1', 900)
-    cardAnimation('.card2', 600)
-    cardAnimation('.card3', 1200)
-    cardAnimation('.card4', 1900)
-    cardAnimation('.card5', 1300)
-    cardAnimation('.card6', 700)
-    cardAnimation('.card7', 400)
-    cardAnimation('.card8', 1400)
+    cardAnimation('.card1', 'loopPopUp', 1200)
+    cardAnimation('.card2 .card-content', 'loopPopUp', 1600)
+    cardAnimation('.card3 .card-content', 'loopPopUp', 800)
+    cardAnimation('.card4 .card-content', 'loopPopUp', 1000)
+    cardAnimation('.card5 .card-content', 'loopPopUp', 1400)
+    cardAnimation('.card6 .card-content', 'loopPopUp', 600)
+    cardAnimation('.card7 .card-content', 'loopPopUp', 2000)
+    cardAnimation('.card8', 'loopPopUp', 1800)
+    cardAnimation('.features-content .card1', 'loopPopUp', 1400)
+    cardAnimation('.features-content .card2 .card-content', 'loopPopUp', 1800)
+    cardAnimation('.features-content .card3 .card-content', 'loopPopUp', 600)
+    cardAnimation('.features-content .card4 .card-content', 'loopPopUp', 1200)
+    cardAnimation('.features-content .card5 .card-content', 'loopPopUp', 1600)
+    cardAnimation('.features-content .card6 .card-content', 'loopPopUp', 800)
+    cardAnimation('.features-content .card7 .card-content', 'loopPopUp', 1000)
+    cardAnimation('.features-content .card8', 'loopPopUp', 2000)
+    cardAnimation('.product1 .product-content', 'rollDown', 1200, 6.8)
+    cardAnimation('.product2 .product-content', 'rollDown', 1000, 6.8)
+    cardAnimation('.product3 .product-content', 'rollDown', 800, 6.8)
 }
 
 // <==========({[Smooth Scroll Handler]})==========>
